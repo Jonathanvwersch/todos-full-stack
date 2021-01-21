68c572e3f3885ee17794ec9d2ec135fe190ee953
Jonathan van Wersch <38623677+Jonathanvwersch@users.noreply.github.com>
2021-01-21 11:52:44 +0100

    completed server...onto client

--- server/server.js
+++ server/server.js
@@ -11,10 +11,9 @@ app.use(express.json())
 app.get("/api/v1/todos", async (req, res) => {
     try {
         const todos = await db.query("select * from todos")
-        console.log(todos)
         res.status(200).json({
             status:"success",
-            results:todos.rows.length,
+            results: todos.rows.length,
             data: {
                 todos: todos.rows
             }
@@ -26,16 +25,70 @@ app.get("/api/v1/todos", async (req, res) => {
 })
 
 // Get a single todo
-app.get("/api/v1/todos", async (req, res))
+app.get("/api/v1/todos/:id", async (req, res) => {
+    try {
+        const todo = await db.query("select * from todos where id=$1", [req.params.id])
+        res.status(200).json({
+            status:"success", 
+            data: {
+                todo: todo.rows[0],
+            }
+        })
 
+    } catch (err) {
+        console.log(err)
+    }
+})
 
 // Create a todo
+app.post("/api/v1/todos", async (req, res) => {
+    try {
+        const results = await db.query(
+            "INSERT INTO todos (todo) values ($1) returning *", [req.body.todo]
+        )
+        res.status(201).json({
+            status: "success", 
+            data: {
+                todo: results.rows[0]
+            }
+        })
+
+    } catch (err) {
+        console.log(err)
+    }
+})
 
 
 // Update todos
+app.put("/api/v1/todos/:id", async (req, res) => {
+    try {
+        const results = await db.query(
+            "UPDATE todos SET todo=$1 RETURNING *",
+            [req.body.todo]
+        )
+        res.status(200).json({
+            status:"success",
+            data: {
+                todos: results.rows[0]
+            }
+        })
 
+    } catch(err) {
+        console.log(err)
+    }
+})
 
 // Delete todos
+app.delete("/api/v1/todos/:id", async (req, res) => {
+    try {
+        const results = await db.query("DELETE FROM todos WHERE id=$1", [req.params.id])
+        res.status(204).json({
+            status:"success"
+        })
+    } catch(err) {
+        console.log(err)
+    }
+})
 
 
 const port = process.env.PORT || 3008
